aa053d397fc4f874e8deaf4977afbdc073712634
Billy <longnd1@bravebits.vn>
2024-11-24 12:06:50 +0700

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,31 @@
-// import { ValidationPipe } from '@nestjs/common';
-// import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
+  const configService = app.get(ConfigService);
+  const allowedOrigins = configService.get('ALLOWED_ORIGINS')?.split(',');
 
   app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
+    origin: allowedOrigins || ['https://quan-ly-sang-kien.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
 
-  await app.listen(3000);
+  // Global pipes for validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
-  // const app = await NestFactory.create(AppModule);
-  // const configService = app.get(ConfigService);
-
-  // app.enableCors({
-  //   origin: ['https://quan-ly-sang-kien.vercel.app'],
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  //   allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
-  //   credentials: true,
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 204,
-  // });
-
-  // // Global pipes for validation
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //     transform: true,
-  //   }),
-  // );
-
-  // // Start server
-  // await app.listen(configService.get('PORT'));
+  // Start server
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
