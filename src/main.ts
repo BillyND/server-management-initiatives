629509d8818d31b2e433b892651d54bd102bf05a
Billy <longnd1@bravebits.vn>
2024-11-24 11:52:24 +0700

    FIx cors

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,34 @@
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+// import { ValidationPipe } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+  app.enableCors();
+  await app.listen(3000);
 
-  app.enableCors({
-    origin: ['https://quan-ly-sang-kien.vercel.app'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+  // const app = await NestFactory.create(AppModule);
+  // const configService = app.get(ConfigService);
 
-  // Global pipes for validation
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
+  // app.enableCors({
+  //   origin: ['https://quan-ly-sang-kien.vercel.app'],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+  //   credentials: true,
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  // });
 
-  // Start server
-  await app.listen(configService.get('PORT'));
+  // // Global pipes for validation
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //     transform: true,
+  //   }),
+  // );
+
+  // // Start server
+  // await app.listen(configService.get('PORT'));
 }
 bootstrap();
