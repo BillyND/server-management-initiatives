2a2998fc606ddd04d3ff52cb87c913752c6fc168
Billy <longnd1@bravebits.vn>
2024-11-25 19:44:22 +0700

    Create route to seed data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PermissionsService } from './modules/permissions/permissions.service';
+import { RolesService } from './modules/roles/roles.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly permissionsService: PermissionsService,
+    private readonly rolesService: RolesService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('seed')
+  async seed() {
+    await this.permissionsService.seedDefaultPermissions();
+    await this.rolesService.seedDefaultRoles();
   }
 }
