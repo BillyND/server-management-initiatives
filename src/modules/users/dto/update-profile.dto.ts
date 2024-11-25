36f09de58e80dbbd4368d92e090445edfa7bc4db
Billy <longnd1@bravebits.vn>
2024-11-26 00:28:07 +0700

    Fix bug can not update profile

--- src/modules/users/dto/update-profile.dto.ts
+++ src/modules/users/dto/update-profile.dto.ts
@@ -1,27 +1,38 @@
-import { IsBoolean, IsEmail, IsOptional, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+  MaxLength,
+} from 'class-validator';
 
 export class UpdateProfileDto {
   @IsString()
-  @IsOptional()
-  name?: string;
+  @Length(2, 50, { message: 'Full name must be between 2 and 50 characters' })
+  name: string;
 
   @IsString()
-  @IsOptional()
+  @Length(10, 15, {
+    message: 'Phone number must be between 10 and 15 characters',
+  })
+  @Matches(/^[0-9+\-\s()]+$/, {
+    message:
+      'Phone number can only contain numbers and the characters +, -, (), space',
+  })
   phone?: string;
 
-  @IsString()
   @IsOptional()
+  @IsString()
+  @MaxLength(50, { message: 'Department must not exceed 50 characters' })
   department?: string;
 
-  @IsString()
   @IsOptional()
+  @IsString()
+  @MaxLength(50, { message: 'Position must not exceed 50 characters' })
   position?: string;
 
   @IsBoolean()
   @IsOptional()
   isActive?: boolean;
-
-  @IsEmail()
-  @IsOptional()
-  email?: string;
 }
